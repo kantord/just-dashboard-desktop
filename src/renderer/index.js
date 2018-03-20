5d921dafbec0432921fe350abfdf5eae745ae82d
Dániel Kántor <git@daniel-kantor.com>
2018-03-20 20:59:12 +0100

    feat: Render Hello World

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,2 +1,20 @@
-// Initial welcome page. Delete the following line to remove it.
-'use strict';const styles=document.createElement('style');styles.innerText=`@import url(https://unpkg.com/spectre.css/dist/spectre.min.css);.empty{display:flex;flex-direction:column;justify-content:center;height:100vh;position:relative}.footer{bottom:0;font-size:13px;left:50%;opacity:.9;position:absolute;transform:translateX(-50%);width:100%}`;const vueScript=document.createElement('script');vueScript.setAttribute('type','text/javascript'),vueScript.setAttribute('src','https://unpkg.com/vue'),vueScript.onload=init,document.head.appendChild(vueScript),document.head.appendChild(styles);function init(){Vue.config.devtools=false,Vue.config.productionTip=false,new Vue({data:{versions:{electron:process.versions.electron,electronWebpack:require('electron-webpack/package.json').version}},methods:{open(b){require('electron').shell.openExternal(b)}},template:`<div><div class=empty><p class="empty-title h5">Welcome to your new project!<p class=empty-subtitle>Get started now and take advantage of the great documentation at hand.<div class=empty-action><button @click="open('https://webpack.electron.build')"class="btn btn-primary">Documentation</button> <button @click="open('https://electron.atom.io/docs/')"class="btn btn-primary">Electron</button><br><ul class=breadcrumb><li class=breadcrumb-item>electron-webpack v{{ versions.electronWebpack }}</li><li class=breadcrumb-item>electron v{{ versions.electron }}</li></ul></div><p class=footer>This intitial landing page can be easily removed from <code>src/renderer/index.js</code>.</p></div></div>`}).$mount('#app')}
+const dashboard = require('just-dashboard')
+'use strict';
+
+const render_dashboard = (data) =>
+  dashboard.json_parser(data)(dashboard.d3.selection())
+
+const render_dashboard_yaml = (data) =>
+  render_dashboard(dashboard.yaml_parser(data))
+
+render_dashboard_yaml(`
+dashboard "Hello World":
+  - h1 text: "Hello from just-dashboard"
+  - pie chart: {
+    "columns": [
+      ["foo", 3],
+      ["bar", 4],
+      ["baz", 2],
+    ]
+  }
+`)

39be733ddc51a1d64b1d135b10c6699b0882efea
Dániel Kántor <git@daniel-kantor.com>
2018-03-20 22:05:55 +0100

    feat: Open file from command line argument

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,5 +1,6 @@
-const dashboard = require('just-dashboard')
 'use strict';
+const dashboard = require('just-dashboard')
+const fs = require('fs')
 
 const render_dashboard = (data) =>
   dashboard.json_parser(data)(dashboard.d3.selection())
@@ -7,14 +8,9 @@ const render_dashboard = (data) =>
 const render_dashboard_yaml = (data) =>
   render_dashboard(dashboard.yaml_parser(data))
 
-render_dashboard_yaml(`
-dashboard "Hello World":
-  - h1 text: "Hello from just-dashboard"
-  - pie chart: {
-    "columns": [
-      ["foo", 3],
-      ["bar", 4],
-      ["baz", 2],
-    ]
-  }
-`)
+
+const argv = require('electron').remote.process.argv
+const input_file = argv[1]
+const file_contents = fs.readFileSync(input_file, {"encoding": "utf-8"})
+
+render_dashboard_yaml(file_contents)

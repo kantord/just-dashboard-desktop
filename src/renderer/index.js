e9b99938c7ee55bfde2ae424f62c971194b93041
Dániel Kántor <git@daniel-kantor.com>
2018-03-25 19:20:05 +0200

    feat: Load files from disk

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -2,16 +2,27 @@
 import * as dashboard from 'just-dashboard/dist/dashboard.js'
 const fs = require('fs')
 
-const render_dashboard = (data) =>
-  dashboard.json_parser(data)(dashboard.d3.selection())
 
-const render_dashboard_yaml = (data) =>
-  render_dashboard(dashboard.yaml_parser(data))
 
 
 const argv = require('electron').remote.process.argv
 const input_file = argv[1]
 const file_contents = fs.readFileSync(input_file, {"encoding": "utf-8"})
+
+const file_loader = (path, callback) => {
+  console.log("Loading file", path)
+  const file_contents = fs.readFileSync(
+    path.slice('file://'.length), {"encoding": "utf-8"})
+  callback(undefined, file_contents)
+  console.log("Loaded file", path)
+}
+
+
+const render_dashboard = (data) =>
+  dashboard.json_parser(data, file_loader)(dashboard.d3.selection())
+
+const render_dashboard_yaml = (data) =>
+  render_dashboard(dashboard.yaml_parser(data))
 
 render_dashboard_yaml(file_contents)
 

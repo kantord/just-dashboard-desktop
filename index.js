f5930b8b790836841a99e9d084d892888fcdc956
Dániel Kántor <git@daniel-kantor.com>
2019-08-15 18:51:52 +0200

    feat: simplify project

---
+++ index.js
@@ -0,0 +1,10 @@
+const electron = require("electron");
+const proc = require("child_process");
+
+// will print something similar to /Users/maf/.../Electron
+console.log(electron);
+
+const process = require("process");
+const input_file = process.argv[2];
+// spawn Electron
+const child = proc.spawn(electron, ["./main.js", input_file]);
---
+++ renderer.js
@@ -0,0 +1,29 @@
+"use strict";
+const dashboard = require("just-dashboard/dist/dashboard.js");
+const fs = require("fs");
+
+const argv = require("electron").remote.process.argv;
+const input_file = argv[2];
+const file_contents = fs.readFileSync(input_file, { encoding: "utf-8" });
+
+const file_loader = (path, callback) => {
+  console.log("Loading file", path);
+  const file_contents = fs.readFileSync(path.slice("file://".length), {
+    encoding: "utf-8"
+  });
+  callback(undefined, file_contents);
+  console.log("Loaded file", path);
+};
+
+const render_dashboard = data =>
+  dashboard.json_parser(data, file_loader)(dashboard.d3.selection());
+
+const render_dashboard_yaml = data =>
+  render_dashboard(dashboard.yaml_parser(data));
+
+render_dashboard_yaml(file_contents);
+
+dashboard.d3
+  .select("body")
+  .attr("id", "baseline")
+  .attr("class", "typeset");
